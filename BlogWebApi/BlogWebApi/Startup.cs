3c95746b072c00bbd0e7959db36b8c1be7558c17
irozajac1 <37371441+irozajac1@users.noreply.github.com>
2020-07-19 22:11:42 +0200

    implementing Swagger

--- BlogWebApi/BlogWebApi/Startup.cs
+++ BlogWebApi/BlogWebApi/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace BlogWebApi
 {
@@ -31,15 +32,29 @@ namespace BlogWebApi
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Blogging Platform", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API-Blogging Platform ");
+                //c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
