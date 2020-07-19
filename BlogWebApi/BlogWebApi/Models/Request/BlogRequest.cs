3c56cc78ea19ef4900cebabf495829cda4174040
irozajac1 <37371441+irozajac1@users.noreply.github.com>
2020-07-19 23:24:01 +0200

    set repository patern

---
+++ BlogWebApi/BlogWebApi/Models/Request/BlogRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogWebApi.Models.Request
+{
+    public class BlogRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ BlogWebApi/BlogWebApi/Models/Response/MultipleBlogsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogWebApi.Models.Response
+{
+    public class MultipleBlogsResponse
+    {
+        public List<BlogPost>MultipleBlogs { get; set; }
+        public int NumberOfBlogs { get; set; }
+    }
+}
