c19966257bf94e06800de098cd1f87e5a2c14eec
irozajac1 <37371441+irozajac1@users.noreply.github.com>
2020-07-23 00:32:16 +0200

    blogService

--- BlogWebApi/BlogWebApi/Interface/IBlogService.cs
+++ BlogWebApi/BlogWebApi/Interface/IBlogService.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using BlogWebApi.Models;
+using BlogWebApi.Models.Request;
+using BlogWebApi.Models.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlogWebApi.Interface
 {
-    interface IBlogService
+    public interface IBlogService
     {
+        BlogPost GetBlogPotBySlug(Guid id);
+        MultipleBlogsResponse GetBlogPostByTag(string tagName);
+        BlogPost GetRecentBlog();
+        BlogPost SendBlogPost(BlogRequest blog);
+        BlogPost UpdateBlogPost(BlogRequest blog);
+        void DeleteBlogPost(Guid blogId);
+
     }
 }
---
+++ BlogWebApi/BlogWebApi/Services/BlogService.cs
@@ -0,0 +1,46 @@
+﻿using BlogWebApi.Interface;
+using BlogWebApi.Models;
+using BlogWebApi.Models.Request;
+using BlogWebApi.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogWebApi.Services
+{
+    public class BlogService : IBlogService
+    {
+        public void DeleteBlogPost(BlogPost blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MultipleBlogsResponse GetBlogPostByTag(string tagName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlogPost GetBlogPotBySlug(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlogPost SendBlogPost(BlogRequest blog)
+        {
+            var newBlog = new BlogPost
+            {
+                Title = blog.Title,
+                Description = blog.Description,
+                Body = blog.Body,
+            };
+
+            return newBlog;
+        }
+
+        public BlogPost UpdateBlogPost(BlogRequest blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
